4f27ee55c7d9b1a577a2905726f6dec9778cb0bd
hjkim0102 <102747351+hjkim0102@users.noreply.github.com>
2023-04-04 01:54:44 +0900

    0403:[10773,O]

---
+++ week2/10773.py
@@ -0,0 +1,13 @@
+import sys
+K = int(sys.stdin.readline())
+a = []
+
+for i in range(K):
+    num = int(sys.stdin.readline())
+    if num == 0:
+        a.pop()
+
+    else:
+        a.append(num)
+
+print(sum(a))
